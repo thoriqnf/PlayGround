696a7144d3f6866dc88755c5e2cc000c86d67820
thoriq <thoriqnfaizal@gmail.com>
2018-07-04 20:54:18 +0700

    merging

--- playgroundreact/src/components/__tests__/App.test.js
+++ playgroundreact/src/components/__tests__/App.test.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from '../App';
+import CommentBox from '../CommentBox';
 
 
-it ('Shows a Comment Box', () =>{
-  const div = document.createElement('div');
-
-  ReactDOM.render(<App/>, div);
-
-  expect(div.innerHTML).toContain('Comment Box');
-
-  ReactDOM.unmountComponentAtNode(div);
+it('Shows a Comment Box', () => {
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(CommentBox).length).toEqual(1);
 });
