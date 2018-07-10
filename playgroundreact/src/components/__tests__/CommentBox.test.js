36dca66b342500130154e1d1b940c810daa6f196
thoriq <thoriqnfaizal@gmail.com>
2018-07-10 15:04:30 +0700

    First test at CommentBox

--- playgroundreact/src/components/__tests__/CommentBox.test.js
+++ playgroundreact/src/components/__tests__/CommentBox.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
-
+import { mount } from 'enzyme';
 import CommentBox from 'components/CommentBox';
 
 it ('has a text area and a button', () => {
-  
+  const wrapped = mount(<CommentBox />);
+
+  console.log(wrapped.find('textarea'));
+  console.log(wrapped.find('button'));
 });
