e0fc9a798a56a829a1f032adf6d5b4817a5ef4de
thoriq <thoriqnfaizal@gmail.com>
2018-07-12 22:15:56 +0700

    Adding .update

--- playgroundreact/src/components/__tests__/CommentBox.test.js
+++ playgroundreact/src/components/__tests__/CommentBox.test.js
@@ -6,12 +6,20 @@ let wrapped;
 
 beforeEach(() => {
   wrapped = mount (<CommentBox/>);
-})
+});
 
 afterEach(() =>{
   wrapped.unmount();
-})
+});
+
 it ('has a text area and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
+});
+
+it('has a text area that users can type in'. () => {
+ wrapped.find('textarea').simulate('change', {
+    target: { value: 'new comment' }
+ });
+ wrapped.update();
 });
