7ba09f96a45d29ac067d2d88ea12ae9d99eb0a66
thoriq <thoriqnfaizal@gmail.com>
2018-07-12 22:30:32 +0700

    Adding props to simulate testing

--- playgroundreact/src/components/__tests__/CommentBox.test.js
+++ playgroundreact/src/components/__tests__/CommentBox.test.js
@@ -17,9 +17,10 @@ it ('has a text area and a button', () => {
   expect(wrapped.find('button').length).toEqual(1);
 });
 
-it('has a text area that users can type in'. () => {
+it('has a text area that users can type in', () => {
  wrapped.find('textarea').simulate('change', {
     target: { value: 'new comment' }
  });
  wrapped.update();
+ expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
 });
