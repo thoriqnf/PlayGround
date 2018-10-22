555e7e564509b8d200dcac4dafac2dee6cc4f501
thoriq <thoriqnfaizal@gmail.com>
2018-10-22 13:20:10 +0700

    Not using babel but using jsx

--- IndecisionApp/public/scripts/app.js
+++ IndecisionApp/public/scripts/app.js
@@ -1,1 +1,11 @@
-console.log("App.js is running");
+console.log("App.js is running!");
+
+
+var template = React.createElement(
+  "h1",
+  { id: "someid" },
+  "Something new"
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
