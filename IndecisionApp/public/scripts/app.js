91ee7b971b8134621e1d80c110f819b19d98824e
thoriq <thoriqnfaizal@gmail.com>
2018-10-24 20:22:55 +0700

    Make nested file inside 1 div

---
+++ IndecisionApp/public/scripts/app.js
@@ -0,0 +1,36 @@
+"use strict";
+
+console.log("App.js is running!");
+
+var template = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "Indecision App"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "This is come info"
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "Item One"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Item Two"
+    )
+  )
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
--- IndecisionApp/src/app.js
+++ IndecisionApp/src/app.js
@@ -1,7 +1,17 @@
 console.log("App.js is running!");
 
 
-var template = <p>This is jsx from app.js!! does this changed</p>
+var template = (
+    <div>
+      <h1>Indecision App</h1>
+      <p>This is come info</p>
+      <ol>
+        <li>Item One</li>
+        <li>Item Two</li>
+      </ol>
+    </div>
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
