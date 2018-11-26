ed15d23b20efc2c546a8a85cc443d78381d168e0
thoriq <thoriqnfaizal@gmail.com>
2018-11-26 23:36:38 +0700

    Changing JSX

--- sgreact/src/index.js
+++ sgreact/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const App = () => {
+  return (
+    <div>
+      <label class="label" for="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style="background-color: blue; color : white">Submit</button>
+    </div>
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.querySelector('#root'));
