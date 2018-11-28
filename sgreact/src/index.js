12bcb67883c047592702a8af3ff10fbf7dbc4768
thoriq <thoriqnfaizal@gmail.com>
2018-11-28 15:10:23 +0700

    Adding CDN semantic ui

---
+++ sgreact/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div>Hani Sayang</div>
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'))
