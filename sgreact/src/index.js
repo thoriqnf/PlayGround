63a8bac2a9c73f1033443a64003f96d10b433a26
thoriq <thoriqnfaizal@gmail.com>
2018-12-01 14:29:56 +0700

    Adding props for avatar

--- sgreact/src/index.js
+++ sgreact/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" />
-      <CommentDetail author="Alex" timeAgo="Today at 2:00AM" />
-      <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" />
-      <CommentDetail author="Thoriq" timeAgo="Last week at 0:00AM" />
+      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content="Nice blog 1" avatar={faker.image.avatar()} />
+      <CommentDetail author="Alex" timeAgo="Today at 2:00AM" content="Nice blog 2" avatar={faker.image.avatar()}/>
+      <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" content="Nice blog 3" avatar={faker.image.avatar()}/>
+      <CommentDetail author="Thoriq" timeAgo="Last week at 0:00AM" content="Nice blog 4" avatar={faker.image.avatar()}/>
     </div>
   )
 };
