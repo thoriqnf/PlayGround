9c4e6ff816a8f5d142b9cc9f7c263e169b73a831
thoriq <thoriqnfaizal@gmail.com>
2018-12-01 14:21:13 +0700

    Adding props for time

--- sgreact/src/index.js
+++ sgreact/src/index.js
@@ -5,10 +5,10 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
-      <CommentDetail author="Thoriq" />
+      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" />
+      <CommentDetail author="Alex" timeAgo="Today at 2:00AM" />
+      <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" />
+      <CommentDetail author="Thoriq" timeAgo="Last week at 0:00AM" />
     </div>
   )
 };
