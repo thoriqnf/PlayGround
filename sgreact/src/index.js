792c1f6c985918db5bbac0790bbd6ee26217c66c
thoriq <thoriqnfaizal@gmail.com>
2018-12-02 17:43:15 +0700

    Ready setup for seasondisplay App

--- sgreact/src/index.js
+++ sgreact/src/index.js
@@ -1,30 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
-import ApprovalCard from "./ApprovalCard";
+
+import SeasonDisplays from './SeasonDisplay';
 
 const App = () => {
   return (
-    <div className="ui container comments">
-      <ApprovalCard>
-        <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content="Nice blog 1" avatar={faker.image.avatar()} />
-      </ApprovalCard>
-
-      <ApprovalCard>
-        <CommentDetail author="Alex" timeAgo="Today at 2:00AM" content="Nice blog 2" avatar={faker.image.avatar()} />
-      </ApprovalCard>
-
-      <ApprovalCard>
-        <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" content="Nice blog 3" avatar={faker.image.avatar()} />
-      </ApprovalCard>
-
-      <ApprovalCard>
-        <CommentDetail author="Thoriq" timeAgo="Last week at 0:00AM" content="Nice blog 4" avatar={faker.image.avatar()} />
-      </ApprovalCard>
-
+    <div>
+      <SeasonDisplays />
     </div>
-  )
+  );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
