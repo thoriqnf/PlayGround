72af12ba1beb1577e6a09c1178b8daf1bf79c756
thoriq <thoriqnfaizal@gmail.com>
2018-12-04 23:47:20 +0700

    Addding initialize state

--- sgreact/src/index.js
+++ sgreact/src/index.js
@@ -4,13 +4,19 @@ import ReactDOM from 'react-dom';
 //import SeasonDisplays from './SeasonDisplay';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: 40 };
+  }
+
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
       (err) => console.log(err)
     );
 
-    return <div>Latitude: </div>
+    return <div>Latitude: {this.state.lat}</div>
   }
 }
 
