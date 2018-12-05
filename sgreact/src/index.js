4b27c559d1c0f2c017605c87a2dbbcf6427ebce1
thoriq <thoriqnfaizal@gmail.com>
2018-12-05 15:03:24 +0700

    Adding lifecycle method didupdate and didmount

--- sgreact/src/index.js
+++ sgreact/src/index.js
@@ -7,18 +7,51 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: 40 };
+    console.log('constructor');
+
+    this.state = {
+      lat: null,
+      errorMessage: ''
+    };
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({ lat: position.coords.latitude })
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
+
+
+  componentDidUpdate() {
+    console.log('Updated');
+  }
+
+  componentDidMount() {
+    console.log('Mount');
+  }
+
   render() {
-    return <div>Latitude: {this.state.lat}</div>
+    if (this.state.errorMessage && !this.state.lat) {
+      return (
+        <div>
+          Error: {this.state.errorMessage}
+        </div>
+      )
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return (
+        <div>
+          Latitude: {this.state.lat}
+        </div>
+      )
+    }
+
+    return <div>Loading cuy!</div>
   }
 }
 
