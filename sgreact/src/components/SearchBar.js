4a32d6f29746b8ebef1dac6bc410c829b57a156f
thoriq <thoriqnfaizal@gmail.com>
2018-12-06 14:48:45 +0700

    Adding prevent default for form refreshing after enter

--- sgreact/src/components/SearchBar.js
+++ sgreact/src/components/SearchBar.js
@@ -4,10 +4,15 @@ class SearchBar extends React.Component {
 
   state = { term: '' }
 
+  onFormSubmit(e) {
+    e.preventDefault();
+
+    console.log(this.state.term);
+  };
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image search</label>
             <input
