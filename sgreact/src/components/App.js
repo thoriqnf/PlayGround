6006f5dc95d44161740ad55fdf42fcceead08a2a
thoriq <thoriqnfaizal@gmail.com>
2018-12-06 17:25:06 +0700

    Axios get from unsplash dev

--- sgreact/src/components/App.js
+++ sgreact/src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 
 class App extends React.Component {
 
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: {
+        query: term
+      },
+      headers: {
+        Authorization: 'Client-ID 42d520b941b51296907e355d942b7efdb08318788891422015e8b686d33c0324'
+      }
+    });
   }
   render() {
     return (
