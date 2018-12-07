c0b38e95dae3b44290546626462545d75974d5cd
thoriq <thoriqnfaizal@gmail.com>
2018-12-07 19:26:01 +0700

    implementing key.id

--- sgreact/src/components/ImageList.js
+++ sgreact/src/components/ImageList.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 const ImageList = (props) => {
-  const images = props.images.map((image) => {
-    return <img src={image.urls.regular} />
+  const ListGambar = props.images.map(({ description, id, urls }) => {
+    return (
+      <div key={id}>
+        <img alt={description} src={urls.regular} />
+      </div>
+    )
   });
 
-  return <div>{images}</div>
+  return <div>{ListGambar}</div>
 }
 
 export default ImageList;
