d9ccf59208316e5792ff14427adeb8616f3430c0
thoriq <thoriqnfaizal@gmail.com>
2018-12-11 11:44:26 +0700

    adding finishing data list song to render

--- sgreact/src/components/App.js
+++ sgreact/src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { selectSong } from '../actions';
-
+// import { selectSong } from '../actions';
+import SongList from './SongList';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <SongList />
+    </div>
   );
-}
+};
 
 export default App;
--- sgreact/src/reducers/index.js
+++ sgreact/src/reducers/index.js
@@ -1,19 +1,18 @@
 import { combineReducers } from 'redux';
 
 const songsReducer = () => {
-  return (
+  return [
     { title: 'No Scrubs', duration: '405' },
     { title: 'Macarena', duration: '2.30' },
     { title: 'All Star', duration: '3.15' },
     { title: 'I want it that way', duration: '1.45' }
-  );
+  ];
 };
 
 const selectedSongReducer = (selectedSong = null, action) => {
   if (action.type === 'SONG_SELECTED') {
     return action.payload;
   }
-
   return selectedSong;
 }
 
