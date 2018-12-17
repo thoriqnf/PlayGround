bbcf7f3f0eece33eca0a5c72fe7c9a50e4d368d2
thoriq <thoriqnfaizal@gmail.com>
2018-12-17 08:06:57 +0700

    Refactoring all userheader

--- sgreact/src/components/App.js
+++ sgreact/src/components/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import PostList from './PostList';
-import UserList from './UserList';
 
 const App = () => {
   return (
     <div className="ui container">
       <PostList />
-      <UserList />
     </div>
   );
 }
--- sgreact/src/components/UserHeader.js
+++ sgreact/src/components/UserHeader.js
@@ -4,28 +4,17 @@ import { fetchUser } from '../actions';
 
 class UserHeader extends React.Component {
   componentDidMount() {
-    console.log(this.props.fetchUser)
     this.props.fetchUser(this.props.userId);
   }
 
-  renderList() {
-    return this.props.users.map(users => {
-      return (
-        <div className="item" key={users.id}>
-          <i className="large middle aligned icon user" />
-          <div className="content">
-            <div className="description">
-              <h2>{users.name}</h2>
-              <p>{users.email}</p>
-            </div>
-          </div>
-        </div>
-      );
-    });
-  }
-
   render() {
-    return <div className="ui relaxed devided list">{this.renderList()}</div>
+    const user = this.props.users.find((user) => user.id === this.props.userId)
+
+    if (!user) {
+      return null;
+    }
+
+    return <div className="header">{user.name}</div>
   }
 }
 
