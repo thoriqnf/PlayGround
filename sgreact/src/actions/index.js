6adffc3507324953d04cb09c860707fa11c9c71f
thoriq <thoriqnfaizal@gmail.com>
2018-12-17 18:50:07 +0700

    Refactoring using _.chain from lodash

--- sgreact/src/actions/index.js
+++ sgreact/src/actions/index.js
@@ -4,9 +4,15 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
   // console.log(getState().posts);
-  const userIds = _.uniq(_.map(getState().posts, 'userId'));
-  userIds.forEach(id => dispatch(fetchUser(id)));
-  console.log(userIds);
+  // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  // userIds.forEach(id => dispatch(fetchUser(id)));
+  // console.log(userIds);
+
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value()
 };
 
 export const fetchPosts = () => async dispatch => {
