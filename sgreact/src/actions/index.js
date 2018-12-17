2eaf456cfadc3610007733e7584e68203f9f04fb
thoriq <thoriqnfaizal@gmail.com>
2018-12-17 17:15:22 +0700

    Adding big action creator to action creator

--- sgreact/src/actions/index.js
+++ sgreact/src/actions/index.js
@@ -1,16 +1,28 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async dispatch => {
+  console.log('about to fetched post');
+  await dispatch(fetchPosts());
+  console.log('fetched posts!');
+};
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
 
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
+// memoize function
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
 
-export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
 
-const _fetchUser = _.memoize(async (id, dispatch) => {
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
+
+export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: 'FETCH_USER', payload: response.data });
-});
+}
