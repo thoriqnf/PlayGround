882b4f0bc417e26ca0b30b8aad3b31ba5a2ea5d1
thoriq <thoriqnfaizal@gmail.com>
2018-12-17 07:43:14 +0700

    Refactoring action user

--- sgreact/src/actions/index.js
+++ sgreact/src/actions/index.js
@@ -6,8 +6,8 @@ export const fetchPosts = () => async dispatch => {
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUsers = () => async dispatch => {
-  const response = await jsonPlaceholder.get('./users');
+export const fetchUser = (id) => async dispatch => {
+  const response = await jsonPlaceholder.get(`'./users/'${id}`);
 
-  dispatch({ type: 'FETCH_USERS', payload: response.data });
+  dispatch({ type: 'FETCH_USER', payload: response.data });
 };
