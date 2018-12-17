dab3b28a749607698c0661c67fe73293c419854b
thoriq <thoriqnfaizal@gmail.com>
2018-12-17 08:42:45 +0700

    Adding logic to mapStateToprops

--- sgreact/src/components/UserHeader.js
+++ sgreact/src/components/UserHeader.js
@@ -8,7 +8,7 @@ class UserHeader extends React.Component {
   }
 
   render() {
-    const user = this.props.users.find((user) => user.id === this.props.userId)
+    const { user } = this.props;
 
     if (!user) {
       return null;
@@ -16,10 +16,10 @@ class UserHeader extends React.Component {
 
     return <div className="header">{user.name}</div>
   }
-}
+};
 
-const mapStateToProps = state => {
-  return { users: state.users }
-}
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find(user => user.id === ownProps.userId) }
+};
 
 export default connect(mapStateToProps, { fetchUser })(UserHeader);
