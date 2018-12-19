691161c3392dafd20a22270a3cab4cbd939090e2
thoriq <thoriqnfaizal@gmail.com>
2018-12-20 06:55:53 +0700

    Wire up route components

--- sgreact/TwitchStreams/client/src/components/App.js
+++ sgreact/TwitchStreams/client/src/components/App.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import { HashRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-const PageOne = () => {
-  return (
-    <div>
-      <Link to="/2">Navigate to 2</Link>
-    </div>
-  );
-};
-
-const PageTwo = () => {
-  return <div>2</div>
-}
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
   return (
     <div>
-      <HashRouter>
+      <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/2" component={PageTwo} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/show" component={StreamShow} />
         </div>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 };
