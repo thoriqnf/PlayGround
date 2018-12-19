2d4739bf4eca6240c64230eab82cda91da82b3ba
thoriq <thoriqnfaizal@gmail.com>
2018-12-19 17:47:10 +0700

    Adding react router and testing a bit

--- sgreact/TwitchStreams/client/src/components/App.js
+++ sgreact/TwitchStreams/client/src/components/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const PageOne = () => {
+  return <div>1</div>
+}
+
+const PageTwo = () => {
+  return <div>2</div>
+}
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/2" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
