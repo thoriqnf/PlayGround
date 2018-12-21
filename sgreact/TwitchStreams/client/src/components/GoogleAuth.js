a59a6fa2e3d5942881a602d51f922c85239c130b
thoriq <thoriqnfaizal@gmail.com>
2018-12-21 14:51:42 +0700

    Make boilerplate googleauth

--- sgreact/TwitchStreams/client/src/components/GoogleAuth.js
+++ sgreact/TwitchStreams/client/src/components/GoogleAuth.js
@@ -1,16 +1,31 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
   componentWillMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
-        clientId: '335469372601 - l3ajv0l23gc4d9l7d910q908rq0s67sk.apps.googleusercontent.com',
+        clientId: '335469372601-l3ajv0l23gc4d9l7d910q908rq0s67sk.apps.googleusercontent.com',
         scope: 'email'
-      });
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+      })
     });
   }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I dont know if we are signed in</div>
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in!</div>
+    } else {
+      return <div>I am not signed in!</div>
+    }
+  }
+
   render() {
-    return <div>Google Auth</div>
+    return <div>{this.renderAuthButton()}</div>
   };
 };
 
