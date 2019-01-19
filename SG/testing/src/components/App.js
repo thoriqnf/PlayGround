773ed6b4aaeeb4b2bcf864c3bd7d9eafc6a235ae
thoriq <thoriqnfaizal@gmail.com>
2019-01-19 14:02:57 +0700

    Adding component comment box and list

--- SG/testing/src/components/App.js
+++ SG/testing/src/components/App.js
@@ -1,5 +1,13 @@
 import React from "react";
+import CommentBox from "./CommentBox";
+import CommentList from "./CommentList";
 
 export default () => {
-  return <div>Im app</div>;
+  return (
+    <div>
+      Im app
+      <CommentBox />
+      <CommentList />
+    </div>
+  );
 };
