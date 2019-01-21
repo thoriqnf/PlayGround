4fdb5dd541561c312fbd6c8a094d9bc9abde65cd
thoriq <thoriqnfaizal@gmail.com>
2019-01-21 21:35:21 +0700

    Testing reducers

--- SG/testing/src/reducers/__tests__/comments.test.js
+++ SG/testing/src/reducers/__tests__/comments.test.js
@@ -4,10 +4,16 @@ import { SAVE_COMMENT } from "actions/types";
 it("handles actions to type SAVE_COMMENTS", () => {
   const action = {
     type: SAVE_COMMENT,
-    payload: "New comment"
+    payload: "New Comment"
   };
 
   const newState = commentsReducer([], action);
 
   expect(newState).toEqual(["New Comment"]);
+});
+
+it("handles action with unknown type", () => {
+  const newState = commentsReducer([], { type: "" });
+
+  expect(newState).toEqual([]);
 });
