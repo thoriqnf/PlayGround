49ec5f9f18f156336791088e9c52fadd91edf2fe
thoriq <thoriqnfaizal@gmail.com>
2019-01-21 20:21:18 +0700

    Connect redux to commentbox

--- SG/testing/src/components/CommentBox.js
+++ SG/testing/src/components/CommentBox.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import * as actions from "actions";
+import { connect } from "react-redux";
 
 class CommentBox extends Component {
   state = { comment: "" };
@@ -9,8 +11,7 @@ class CommentBox extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
-
-    // Call an action creator and save the comment
+    this.props.saveComment(this.state.comment);
     this.setState({ comment: "" });
   };
   render() {
@@ -26,4 +27,7 @@ class CommentBox extends Component {
   }
 }
 
-export default CommentBox;
+export default connect(
+  null,
+  actions
+)(CommentBox);
