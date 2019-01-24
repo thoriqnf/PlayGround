58344e2f59aaff187473e47406665f58ee6e1230
thoriq <thoriqnfaizal@gmail.com>
2019-01-24 16:23:32 +0700

    Fixing moxios issue

--- SG/testing/src/__tests__/integrations.test.js
+++ SG/testing/src/__tests__/integrations.test.js
@@ -2,6 +2,19 @@ import React from 'react';
 import { mount } from 'enzyme';
 import Root from 'Root';
 import App from 'components/App';
+import moxios from 'moxios';
+
+beforeEach(() => {
+  moxios.install();
+  moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+    status: 200,
+    response: [{ name: 'Fetch #1' }, { name: 'Fetch #2' }]
+  })
+})
+
+afterEach(() => {
+  moxios.uninstall();
+})
 
 it('Can fetch a list of comments and display them', () => {
   // Attemp to render the entire app
@@ -11,6 +24,15 @@ it('Can fetch a list of comments and display them', () => {
     </Root>
   )
   // FInd the 'fetchComments' button and  click it
+  wrapped.find('.fetch-comments').simulate('click');
 
   // Expect to fin a list of comments! 500 Li's
+  setTimeout(() => {
+    wrapped.update();
+
+    expect(wrapped.find('li').length).toEqual(2);
+
+    done();
+    wrapped.unmount();
+  }, 100)
 });
--- SG/testing/src/components/CommentBox.js
+++ SG/testing/src/components/CommentBox.js
@@ -24,7 +24,11 @@ class CommentBox extends Component {
             <button>Submit Comment</button>
           </div>
         </form>
-        <button onClick={this.props.fetchComments}>Fetch Comments</button>
+        <button
+          className="fetch-comments"
+          onClick={this.props.fetchComments}>
+          Fetch Comments
+        </button>
       </div>
     );
   }
