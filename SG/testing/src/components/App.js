efa49e556a2896be0668bbde6fe7f5efbbfb97c1
thoriq <thoriqnfaizal@gmail.com>
2019-02-04 00:18:10 +0700

    Adding header for sign in and sign out

--- SG/testing/src/components/App.js
+++ SG/testing/src/components/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
-import { connect } from 'react-router';
+import { Route, Link } from "react-router-dom";
+import { connect } from 'react-redux';
 import CommentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
 
-export default class App extends Component {
+class App extends Component {
 
   renderButton() {
     if (this.props.auth) {
@@ -39,9 +39,16 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        {this.renderHeader()}
         <Route path="/post" component={CommentBox} />
         <Route path="/" exact component={CommentList} />
       </div >
     );
   }
 };
+function mapStatetoProps(state) {
+  return { auth: state.auth };
+}
+
+
+export default connect(mapStatetoProps)(App);
