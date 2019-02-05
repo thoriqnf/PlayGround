c6f0747a802625b1e2c28c29f5c68b520e35b563
thoriq <thoriqnfaizal@gmail.com>
2019-02-05 21:18:13 +0700

    Adding logic to re render to home if not sign in

--- SG/testing/src/components/CommentBox.js
+++ SG/testing/src/components/CommentBox.js
@@ -18,6 +18,7 @@ class CommentBox extends Component {
   shouldNavigateAway() {
     if (!this.props.auth) {
       console.log('I NEED TO LEAVE!!!');
+      this.props.history.push('/');
     }
   }
 
